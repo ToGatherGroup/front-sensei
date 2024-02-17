ec376a5cf0a87228d9ff79f3e90988b92cdbfe85
Bruno <bru-a@hotmail.com>
2024-02-17 01:01:17 -0300

    Initializing Cadastro Atleta

---
+++ src/app/atleta/cadastrar/page.tsx
@@ -0,0 +1,13 @@
+import styles from "./cadastrarAtleta.module.css";
+import FormAtleta from "@/app/components/form_atleta";
+
+type Props = {};
+const CadastrarAtleta = (props: Props) => {
+  return (
+    <div>
+      <FormAtleta />
+    </div>
+  );
+};
+
+export default CadastrarAtleta;
