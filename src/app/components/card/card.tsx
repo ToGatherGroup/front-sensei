98932868e678ba2948fded79c19ab2292b312f5e
Bruno <bru-a@hotmail.com>
2024-02-20 01:11:29 -0300

    Cards menu

---
+++ src/app/components/card/card.tsx
@@ -0,0 +1,36 @@
+type Props = {
+  cardTitle: string;
+  src: string;
+  width?: number;
+  height?: number;
+};
+
+import Image from "next/image";
+
+import styles from "./card.module.css";
+
+// Default size of cards icon images (if passed as prop, it'll be ignored)
+const defaultWidth = 180;
+const defaultHeight = 180;
+
+const card = ({ cardTitle, src, width, height }: Props) => {
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.cardTitle}>
+        <h2>{cardTitle}</h2>
+      </div>
+
+      <div className={styles.icon}>
+        <figure>
+          <Image
+            src={src}
+            alt={`Ícone do ${cardTitle}`}
+            width={width ?? defaultWidth}
+            height={height ?? defaultHeight}
+          />
+        </figure>
+      </div>
+    </div>
+  );
+};
+export default card;
