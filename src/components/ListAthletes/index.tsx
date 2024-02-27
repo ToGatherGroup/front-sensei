12c596d60ac9ebc57e5512fa352f3d28ea738faa
Felipe Marcelino <felipe99marcelino@hotmail.com>
2024-02-26 21:43:17 -0300

    chore: add key on map

--- src/components/ListAthletes/index.tsx
+++ src/components/ListAthletes/index.tsx
@@ -15,7 +15,7 @@ export default function ss({ listAthletes } : ListAthletesProps){
             <h2 className="font-bold uppercase text-4xl mb-10">Lista de Atletas</h2>
             <ul className="w-full">
                 {listAthletes.map((athlete) => (
-                    <li className="flex items-center justify-start gap-2 mb-3">
+                    <li className="flex items-center justify-start gap-2 mb-3" key={athlete.id}>
                         <input id="idAthlete" type="checkbox" value={athlete.id} className="w-6 h-6 bg-gray-100 rounded-md border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
                         <span className="uppercase text-lg whitespace-nowrap">{athlete.name}</span>
                     </li>
