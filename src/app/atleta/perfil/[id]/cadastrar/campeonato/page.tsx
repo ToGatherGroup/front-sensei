1f27ed0efb4c18aeb01b6c7a87ae628966747901
charlesanderson25 <charles_spfc@hotmail.com>
2024-03-25 13:45:32 -0300

    Add form and styles

--- src/app/atleta/perfil/[id]/cadastrar/campeonato/page.tsx
+++ src/app/atleta/perfil/[id]/cadastrar/campeonato/page.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/button";
 import styles from "./campeonatos.module.css";
 
 import FormTitle from "@/components/Title/formTitle/index";
@@ -5,12 +6,67 @@ import FormTitle from "@/components/Title/formTitle/index";
 type Props = {};
 const atletaCampeonatos = (props: Props) => {
   return (
-    <div style={{ display: "flex" }}>
+    // <div style={{ display: "flex" }}>
+    <div className="flex justify-center items-center">
       <div className="form-container">
-        <FormTitle
-          title="Cadastrar Campeonatos"
-          iconSrc="/icons/trophy_24x24.png"
-        />
+        <div className="flex justify-center items-center">
+          <FormTitle
+            title="Cadastrar Campeonatos"
+            iconSrc="/icons/trophy_24x24.png"
+          />
+        </div>
+        <div className="flex justify-center my-40">
+          <form action="">
+            <div className="mx-auto my-6 box-border">
+              <label
+                htmlFor="name"
+                className="inline-block w-48 text-center text-base font-semibold"
+              >
+                Nome do Campeonato
+              </label>
+              <input
+                type="text"
+                className="bg-gray-200 w-72 px-4 py-2 rounded"
+                placeholder="Informe o nome do campeonato"
+              />
+            </div>
+
+            <div className="mx-auto my-6 box-border">
+              <label
+                htmlFor="name"
+                className="inline-block w-48 text-center text-base font-semibold"
+              >
+                Data do Campeonato
+              </label>
+              <input
+                type="date"
+                className="bg-gray-200 w-72 px-4 py-2 rounded"
+              />
+            </div>
+
+            <div className="mx-auto my-6 box-border">
+              <label
+                htmlFor="name"
+                className="inline-block w-48 text-center text-base font-semibold"
+              >
+                Posição
+              </label>
+              <select
+                name=""
+                id=""
+                className="bg-gray-200 w-72 px-4 py-2 rounded"
+              >
+                <option value="first">Primeiro Colocado</option>
+                <option value="second">Segundo Colocado</option>
+                <option value="third">Terceiro Colocado</option>
+                <option value="competed">Competiu</option>
+              </select>
+            </div>
+            <div className="my-20">
+              <Button label="cadastrar" type="submit" />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
