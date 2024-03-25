77c405f469e6e2ef5ff2cb4d9b999ad6145351dc
Felipe Marcelino <felipe99marcelino@hotmail.com>
2024-03-25 20:25:25 -0300

    chore: add svg frontal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Injuries from "@/components/injuries/index";
+
 export default function Home() {
   return (
    <main className="h-screen flex items-center justify-center">
       <h1 className="text-white text-[50px] font-bold">Sensei Divino</h1>
+      <Injuries />
    </main>
   );
 }
