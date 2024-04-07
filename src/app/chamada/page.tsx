5a01838269676ae54639f8c661f86bef6b7ed60f
charlesanderson25 <charles_spfc@hotmail.com>
2024-04-07 20:00:38 -0300

    Add new file

---
+++ src/app/chamada/page.tsx
@@ -0,0 +1,13 @@
+import { deflate } from "zlib";
+
+type Props = {};
+
+const Chamada = (props: Props) => {
+  return (
+    <div>
+      <h1>Página em Construção!</h1>
+    </div>
+  );
+};
+
+export default Chamada;
