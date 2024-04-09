94ca3f59ef887e06428495e1d488f36f15f67a3e
Paulo Mota <paulo.kiin@live.com>
2024-04-09 19:42:01 -0300

    Revert "first commit visual WIP"
    This reverts commit d864b2b9014f98e4691d37e6268f82ebc0149053.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,7 @@
-import AvaliacaoPage from "./valencia/avaliacao/page";
-
-//TODO: Vou ter que receber essa lista de um local comum (extrair para enums ou const)
-import { ListItemProps } from '../components/listItem/index'
-const assesmentArray: ListItemProps[] = [
-  {key: 1, itemTitle: 'Core'},
-  {key: 2, itemTitle: 'Força Máxima'},
-  {key: 3, itemTitle: 'Força Explosiva'},
-  {key: 4, itemTitle: 'Força Isométrica'},
-  {key: 5, itemTitle: 'Mobilidade do Tornozelo'},
-  {key: 6, itemTitle: 'Resistência muscular localizada', subItem: ['Abdominal', 'MMSS']},
-  {key: 7, itemTitle: 'Resistência Anaeróbica'},
-  {key: 8, itemTitle: 'Resistência Aeróbica'}
-]
-
-const indiceArray: ListItemProps[] = [
-{key: 1, itemTitle: 'IMC'},
-]
-
 export default function Home() {
   return (
    <main className="h-screen flex items-center justify-center">
-      <AvaliacaoPage assessmentType="Core"/>
+      <h1 className="text-white text-[50px] font-bold">Sensei Divino</h1>
    </main>
   );
 }
-
-//ListAvaliaçãoPage precisará receber o tipo de avaliação como props
-// para rendererizar o tipo de input e o título correto
-// O título capaz que também será um enum ou const (sepá já pode ser no assesmentArray como outro campo "itemAssessmentType")
