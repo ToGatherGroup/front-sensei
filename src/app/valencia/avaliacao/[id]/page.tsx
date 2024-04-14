05c2094fd5e28c0bc8d426905f4dc9e453d7a694
Paulo Mota <paulo.kiin@live.com>
2024-04-14 14:50:06 -0300

    Type Athelete fixes and API consume, css standardization, remove comments and console.log

--- src/app/valencia/avaliacao/[id]/page.tsx
+++ src/app/valencia/avaliacao/[id]/page.tsx
@@ -1,23 +1,9 @@
-'use client';
+"use client";
 import React from 'react'
 import ListAvaliacao from '../../../../components/listAvaliacao/index'
+import Loading from "@/components/loading/index";
 import { useParams } from 'next/navigation'
-
-// TODO: API integration
-const athletes = [
-    {id: 1, name: 'Felipe Marcelino Do Nascimento'},
-    {id: 2, name: 'Bruno Amado'},
-    {id: 3, name: 'Alex'},
-    {id: 4, name: 'Michel'},
-    {id: 5, name: 'Denilson'},
-    {id: 6, name: 'Maiara'},
-    {id: 7, name: 'Kaka'},
-    {id: 8, name: 'Post Hoc Ergo'},
-    {id: 9, name: 'Lorem Ipsum'},
-    {id: 10, name: 'Dolor Sit Amet'},
-    {id: 11, name: 'Consectetur Adipiscing'},
-    {id: 12, name: 'Propter Hoc'},
-]
+import { useAthleteProvider } from '@/contexts'
 
 // export type AvaliacaoPageProps = {
 //   id: number;
@@ -33,39 +19,17 @@ type Props = {
 };
 
 const ListAvaliacaoPage = ({ params }: Props) => {
-  const parametros = useParams<{ id: string;}>() // index?: string
-  // const router: NextRouter = useRouter();
-  // const { id } = router.query;
-
-  // const avaliacao = AVALIACOES_FISICAS[Number(id)] //TODO: desacoplar o id do array de avaliações
-  // console.log(avaliacao)
-  // console.log(AVALIACOES_FISICAS[Number(id)])
-  console.log("Identificador saindo: ")
-   console.log(parametros.id)
-  // Poh, talvez eu tenha que pegar a const aqui ??
-
-// -----------------------------------------------
-// Doc diz que posso usar assim:
-
-// 'use client'
-// import { useParams } from 'next/navigation'
- 
-// export default function ExampleClientComponent() {
-//   const params = useParams<{ tag: string; item: string }>()
- 
-//   // Route -> /shop/[tag]/[item]
-//   // URL -> /shop/shoes/nike-air-max-97
-//   // `params` -> { tag: 'shoes', item: 'nike-air-max-97' }
-//   console.log(params)
- 
-//   return <></>
-// }
-// -----------------------------------------------
+  const parametros = useParams<{ id: string;}>()
+  const { listAthletes, isLoading }  = useAthleteProvider();
 
   return (
     <div className='min-h-screen w-full flex items-center justify-center'>
       <div className='max-container'>
-        <ListAvaliacao listAthletes={athletes} identificador={parametros.id}  />
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <ListAvaliacao listAthletes={listAthletes} identificador={parametros.id}  />
+        )}
       </div>
     </div>
   )
--- src/components/listAvaliacao/index.tsx
+++ src/components/listAvaliacao/index.tsx
@@ -1,91 +1,118 @@
+'use client'
 import Button from "../button/index";
 import { AVALIACOES_FISICAS, INDICES_FISICOS } from "@/consts/const";
+import { useAthleteProvider } from "@/contexts";
 import { useState } from "react";
+import { ListAthletesProps } from "@/contexts/athlete/athlete.type";
 
 type ListAvaliacaoProps = {
     identificador: number | string;
-    listAthletes: AthletesProps[];
+    listAthletes: ListAthletesProps | null;
     isIMC?: boolean;
     identificadorSubItem?: number;
 }
 
-type AthletesProps = {
-    id: number | string;
-    name: string;
-}
-
+type FormValues = {
+    [key: number]: string;
+};
 
 export default function ListAvaliacao({ listAthletes, isIMC, identificador } : ListAvaliacaoProps){
+    const { call, success, error } = useAthleteProvider()
+    const [formValues, setFormValues] = useState<FormValues>({});
 
-    function getDate() {
+    const handleInputChange = (athleteId: number, value: string) => {
+        setFormValues(prevValues => ({
+            ...prevValues,
+            [athleteId]: value
+        }));
+    };
+
+    const getDate = () => {
         const today = new Date();
         const formatter = new Intl.DateTimeFormat('pt-BR', { month: 'long', year: 'numeric' });
         const formattedDate = formatter.format(today);
         return formattedDate.replace(' de ', '/');
-      }
+    }
 
     const [currentDate, setCurrentDate] = useState(getDate());
 
     let tituloAvaliacao = "Avaliação Física" // Default
     let tipoAvaliacao = "Avaliação Física" // Default
 
-    console.log(`Identificador chegando: ${identificador}`)
-    console.log(`isIMC chegando: ${isIMC}`)
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        // Console.log enquanto não houver integração com API
+        console.log(`identificador: ${identificador}`)
+        console.log(`isIMC: ${isIMC}`)
+        console.log(`currentDate: ${currentDate}`)
+        console.log(`tituloAvaliacao: ${tituloAvaliacao}`)
+        console.log(`tipoAvaliacao: ${tipoAvaliacao}`)
+        console.log(formValues); // Aqui você pode também logar outras informações se necessário
+    };
 
     //TODO: Guardar n'um utils
     const identificadorStr = String(identificador);
     const regex = /^(.+)-(.+)$/; // Regex para identificar se a string contém um hífen no meio
     const match = identificadorStr.match(regex);
-    console.log(`IdentificadorStr: ${identificadorStr}`)
     let identificadorSubItem = 0;
-    console.log(`Macthou?: ${match}`)
     if (match) {
      identificador = Number(match[1]);
      identificadorSubItem = Number(match[2]);
     }
     const assessmentIndex = identificadorSubItem ? identificadorSubItem : 0
 
-
-
     // TODO: refatorar para um switch case -?-
     // TODO: desacoplar/refatorar o id do array de avaliações/indices
-    if (Number(identificador) < Number(8)) {    
-        console.log(`identificador setando AVALIACOES_FISICAS: ${identificador}`)    
+    if (Number(identificador) < Number(8)) {       
         tituloAvaliacao = AVALIACOES_FISICAS[Number(identificador)].assessments[assessmentIndex].title // Validar se tem subItem, caso tenha, pegar segundo parâmetro para navegar na array de assessments
         tipoAvaliacao = AVALIACOES_FISICAS[Number(identificador)].assessments[assessmentIndex].type
         
     } else {
     // TODO: tirar magic number
         identificador = 8; // Hide any possible pagination error
-        console.log(`identificador setando INDICES_FISICOS: ${identificador}`)  
-        tituloAvaliacao = INDICES_FISICOS[0].assessments[assessmentIndex].title // Validar se tem subItem, caso tenha, pegar segundo parâmetro para navegar na array de assessments
+        tituloAvaliacao = INDICES_FISICOS[0].assessments[assessmentIndex].title
         tipoAvaliacao = INDICES_FISICOS[0].assessments[assessmentIndex].type
         isIMC = true
     }
 
     return (
-        <div className="  min-h-screen flex items-center justify-center">
-        <form className="bg-gray-200 p-8 rounded-lg shadow-md  w-90">
-        <h2 className="text-2xl font-semibold mb-6 text-center">{tituloAvaliacao}</h2>
+        <form className="bg-defaultGray px-10 py-6 sm:px-8 sm:py-4 md:px-24 md:py-16 xl:px-36 xl:py-20 rounded-md max-h-[650px] overflow-y-auto custom-scrollbar" onSubmit={handleSubmit} >
+        <h2 className="font-bold uppercase xl:text-4xl md:text-1xl sm:text-lg text-lg xl:mb-10 md:mb-7 sm:mb-5 mb-5 text-center">{tituloAvaliacao}</h2>
 
-        <div className="flex items-center justify-between mb-4">
-            <label className="block text-sm font-medium text-gray-700 mr-2">Data:</label>
-            <input type="text" readOnly className="block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-opacity-50 text-center" value={currentDate} />
+        <div className="flex items-center justify-center mb-4">
+            <label className="block text-gray-700 mr-2">Data:</label>
+            <input type="text" readOnly className="italic text-slate-400 block w-18 border-gray-300 rounded-md shadow-sm focus:ring focus:ring-opacity-50 text-center" value={currentDate} />
         </div>
 
-            <ul className="w-full grid  justify-items-stretch">
-                {listAthletes.map((athlete) => (
-                    <li key={athlete.id} className="flex justify-around items-center py-2 ">
-                        <span className="uppercase text-gray-700 w-40 text-xs font-semibold">{athlete.name}</span>
+            <ul className="w-full xl:mb-10 md:mb-7 sm:mb-5 mb-5">
+                {listAthletes?.data?.map((athlete) => (
+                    <li key={athlete.id} className="flex items-center justify-between xl:gap-4 md:gap-4 sm:gap-4 gap-4 xl:mb-6 md:mb-4 sm:mb-2 mb-2">
+                        <span className="uppercase xl:text-md md:text-base sm:text-sm text-sm ">{athlete?.nome}</span>
                         <div className="flex flex-row space-x-2"> 
-                            {isIMC && <input placeholder="Peso" key={athlete.id} className=" text-sm w-14 h-8 bg-gray-100 rounded-md border-gray-300 placeholder:italic placeholder:text-slate-400 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>}
-                            <input id="idAthlete" type={tipoAvaliacao} className="pl-2 w-14 h-8 bg-gray-100 rounded-md border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                            {isIMC && 
+                            <input 
+                                placeholder="Peso" 
+                                key={athlete?.id} 
+                                className="placeholder:italic placeholder:text-slate-400 xl:w-14 md:w-12 sm:w-8 w-14 xl:h-12 md:h-10 sm:h-6 h-6 bg-gray-100 rounded-md border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                            </input>}
+                            <input 
+                                id="idAthlete" 
+                                type={tipoAvaliacao} 
+                                value={formValues[athlete.id] || ''} 
+                                onChange={(e) => handleInputChange(athlete.id, e.target.value)} 
+                                className="xl:w-14 md:w-12 sm:w-8 w-14 xl:h-8 md:h-10 sm:h-6 h-6 bg-gray-100 rounded-md border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                            </input>
                         </div>
                     </li>
                 ))}
             </ul>
             <Button label="Finalizar Exercício" type="submit" />
+            {success && (
+                <p className="xl:text-base md:text-sm sm:text-xs text-xs text-center text-green-400 xl:mt-10 md:mt-7 sm:mt-5 mt-5 font-bold">{success}</p>
+            )}
+            {error && (
+                <p className="xl:text-base md:text-sm sm:text-xs text-xs text-center text-orange-700 xl:mt-10 md:mt-7 sm:mt-5 mt-5 font-bold">{error}</p>
+            )}
         </form>
-    </div>
     )   
 }
