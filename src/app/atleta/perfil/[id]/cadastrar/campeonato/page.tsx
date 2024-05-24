e10699fb7d7415e12c4baa23024e64c332c21489
wilrrama <wilrrama@hotmail.com>
2024-05-23 21:38:46 -0300

    feat: integração com api

--- src/app/atleta/perfil/[id]/cadastrar/campeonato/page.tsx
+++ src/app/atleta/perfil/[id]/cadastrar/campeonato/page.tsx
@@ -1,13 +1,76 @@
+"use client";
+import { NextPageContext } from "next";
 import Button from "@/components/button";
 import styles from "./campeonatos.module.css";
 import { useForm } from "react-hook-form";
-
 import FormTitle from "@/components/Title/formTitle/index";
+import axios from "axios";
 
-type Props = {};
-const atletaCampeonatos = (props: Props) => {
+// Configuração da API
+const api = axios.create({
+  baseURL: "https://sensei.squareweb.app/",
+});
+
+// Definição do tipo de dados do formulário
+type FormData = {
+  name: string;
+  date: string;
+  position: string;
+  atletaModel: string | number;
+};
+
+// Função para enviar o formulário
+const submitForm = async (data: FormData) => {
+  try {
+    const response = await api.post(
+      "campeonato",
+      {
+        nome: data.name,
+        data: data.date,
+        posicaoPodium: data.position,
+        atletaModel: {
+          id: data.atletaModel,
+        },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (response.status !== 200) {
+      throw new Error("Erro ao cadastrar campeonato");
+    }
+    console.log("Campeonato cadastrado com sucesso!");
+  } catch (error) {
+    console.error("Erro ao cadastrar campeonato Api:", error);
+    throw error;
+  }
+};
+
+// Página de cadastro de campeonatos do atleta
+const AtletaCampeonatos = (atletaId: number | string) => {
+  // ID estático do atleta
+  // const atletaId = "2";
+
+  // Configuração do formulário
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  // Função para submeter o formulário
+  const onSubmit = async (data: FormData) => {
+    try {
+      data.atletaModel = atletaId; // Adiciona o ID do atleta ao dado do formulário
+      await submitForm(data);
+    } catch (error) {
+      console.error("Erro ao cadastrar campeonato Submit:", error);
+    }
+  };
+
   return (
-    // <div style={{ display: "flex" }}>
     <div className="flex justify-center items-center">
       <div className="form-container">
         <div className="flex justify-center items-center">
@@ -17,7 +80,7 @@ const atletaCampeonatos = (props: Props) => {
           />
         </div>
         <div className="flex justify-center my-40">
-          <form action="">
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="mx-auto my-6 box-border">
               <label
                 htmlFor="name"
@@ -26,42 +89,54 @@ const atletaCampeonatos = (props: Props) => {
                 Nome do Campeonato
               </label>
               <input
+                {...register("name", { required: true })}
                 type="text"
                 className="bg-gray-200 w-72 px-4 py-2 rounded"
                 placeholder="Informe o nome do campeonato"
               />
+              {errors.name && (
+                <p className="text-center text-red-500 p-2">
+                  Este campo é obrigatório
+                </p>
+              )}
             </div>
 
             <div className="mx-auto my-6 box-border">
               <label
-                htmlFor="name"
+                htmlFor="date"
                 className="inline-block w-48 text-center text-base font-semibold"
               >
                 Data do Campeonato
               </label>
               <input
+                {...register("date", { required: true })}
                 type="date"
                 className="bg-gray-200 w-72 px-4 py-2 rounded"
               />
+              {errors.date && (
+                <p className="text-center text-red-500 p-2">
+                  Este campo é obrigatório
+                </p>
+              )}
             </div>
 
             <div className="mx-auto my-6 box-border">
               <label
-                htmlFor="name"
+                htmlFor="position"
                 className="inline-block w-48 text-center text-base font-semibold"
               >
                 Posição
               </label>
               <select
-                name=""
-                id=""
+                {...register("position", { required: true })}
                 className="bg-gray-200 w-72 px-4 py-2 rounded"
               >
-                <option value="first">Primeiro Colocado</option>
-                <option value="second">Segundo Colocado</option>
-                <option value="third">Terceiro Colocado</option>
-                <option value="competed">Competiu</option>
+                <option value="PRIMEIRO">Primeiro Colocado</option>
+                <option value="SEGUNDO">Segundo Colocado</option>
+                <option value="TERCEIRO">Terceiro Colocado</option>
+                <option value="PARTICIPACAO">Participação</option>
               </select>
+              {errors.position && <span>Este campo é obrigatório</span>}
             </div>
             <div className="my-20">
               <Button label="cadastrar" type="submit" />
@@ -72,4 +147,5 @@ const atletaCampeonatos = (props: Props) => {
     </div>
   );
 };
-export default atletaCampeonatos;
+
+export default AtletaCampeonatos;
