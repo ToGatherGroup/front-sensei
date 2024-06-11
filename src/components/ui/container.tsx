a52b9615bf12787b50103566e6914404000345e0
Bruno <bru-a@hotmail.com>
2024-06-11 15:33:26 -0300

    now Container component can receive an array of ReactComponents

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -1,14 +1,14 @@
 import { ReactElement } from "react";
 
-type Props = {
-  children: ReactElement;
+type ContainerProps = {
+  children: ReactElement | ReactElement[];
   className?: string | undefined;
 };
-const Container = ({ children, className = "" }: Props) => {
+const Container = ({ children, className = "" }: ContainerProps) => {
   return (
     <div className="p-10 px-5">
       <div
-        className={`m-auto w-auto max-w-[650px] min-h-[80vh] bg-gray-200 rounded p-5 box-border ${className}`}
+        className={`m-auto w-auto max-w-[650px] min-h-fit bg-gray-200 rounded p-5 box-border ${className}`}
       >
         {children}
       </div>
