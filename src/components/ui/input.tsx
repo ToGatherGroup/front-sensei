b97d9e7b1f6c24ed0d78acd882aa97f2f40c8592
Bruno <bru-a@hotmail.com>
2024-06-11 17:30:57 -0300

    add label prop to Input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -15,15 +15,26 @@
 
 type Props = {
   name?: string | undefined;
-  type: "text" | "select" | "date" | "email" | "file" | "password" | "submit";
+  type:
+    | "text"
+    | "select"
+    | "date"
+    | "email"
+    | "file"
+    | "password"
+    | "checkbox"
+    | "submit";
   placeHolder?: string | undefined;
-  size: "tiny" | "small" | "normal" | "fit";
+  label?: string | undefined;
+  required?: boolean | undefined;
+  size: "tiny" | "small" | "normal" | "fit" | "full";
   defaultValue?: string | undefined;
   options?: {
     value: string;
     label: string;
   }[];
-  className?: string | undefined;
+  inputClassName?: string | undefined;
+  labelClassName?: string | undefined;
 };
 
 const SIZES = {
@@ -31,18 +42,27 @@ const SIZES = {
   small: "w-[8rem]",
   normal: "w-[14rem] sm:w-[19rem]",
   fit: "w-fit",
+  full: "w-full",
 };
 
 const Input = ({
   name,
   type,
   placeHolder,
+  label,
+  required,
   size,
   options, // Only for Select type
   defaultValue, // Only for Select type
-  className: outerClassName,
+  inputClassName: outerInputClassName,
+  labelClassName: outerLabelClassName,
 }: Props) => {
-  const className = `bg-gray-300 rounded p-1.5 ${SIZES[size]} ${outerClassName}`;
+  const inputClassName = `h-8 bg-input placeholder:bg-[#9ca3af] mx-1 rounded p-1.5 ${SIZES[size]} ${outerInputClassName}`;
+
+  const labelClassName = `w-full text-center md:w-56 md:min-w-56 p-1 ${
+    required &&
+    "after:content-['*'] after:text-red-600 after:ml-0.5 after:font-extrabold"
+  } ${outerLabelClassName}`;
 
   if (type === "submit")
     throw new Error(
@@ -56,31 +76,45 @@ const Input = ({
 
   if (type === "select")
     return (
-      <select
-        name={name}
-        defaultValue={placeHolder ? "" : defaultValue}
-        className={className}
-      >
-        {placeHolder && (
-          <option value="" disabled hidden>
-            {placeHolder}
-          </option>
+      <div className="flex flex-col my-5 items-center justify-center md:flex-row">
+        {label && (
+          <label htmlFor={name} className={labelClassName}>
+            {label}
+          </label>
         )}
-        {options?.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
+        <select
+          name={name}
+          defaultValue={placeHolder ? "" : defaultValue}
+          className={inputClassName}
+        >
+          {placeHolder && (
+            <option value="" disabled hidden>
+              {placeHolder}
+            </option>
+          )}
+          {options?.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
     );
 
   return (
-    <input
-      name={name}
-      type={type}
-      placeholder={placeHolder}
-      className={`focus:outline focus:outline-2 focus:outline-winePatternLight ${className}`}
-    />
+    <div className="flex flex-col my-5 items-center justify-center md:flex-row md:justify-start">
+      {label && (
+        <label htmlFor={name} className={labelClassName}>
+          {label}
+        </label>
+      )}
+      <input
+        name={name}
+        type={type}
+        placeholder={placeHolder}
+        className={`focus:outline focus:outline-2 focus:outline-winePatternLight ${inputClassName}`}
+      />
+    </div>
   );
 };
 export default Input;
