15ed35f0976b806ab80fa4d1faaa8360fb6b4dda
Bruno <bru-a@hotmail.com>
2024-06-14 17:54:11 -0300

    Add <FormRow/> component to be used with forms

---
+++ src/components/ui/formRow.tsx
@@ -0,0 +1,50 @@
+/*
+
+Exemplo de como usar:
+
+    <FormRow
+    label={
+        <label htmlFor={`coreTime.${index}.value` as const}>
+        {field.atletaNome}
+        </label>
+    }
+
+    input={
+        <input type="text"/>
+    }
+
+    errorMessage={errors.coreTime?.[index]?.value?.message}
+    />
+*/
+
+type FormRowProps = {
+  label: React.ReactElement;
+  input: React.ReactElement;
+  errorMessage?: string | undefined;
+};
+const FormRow = ({ label, input, errorMessage }: FormRowProps) => {
+  const className = {
+    labelChildren:
+      "[&_label]:w-full [&_label]:max-w-full [&_label]:sm:w-72 [&_label]:text-center",
+    inputChildren: `[&_input]:w-24 [&_input]:text-center [&_input]:placeholder:text-center ${
+      errorMessage &&
+      "[&_input]:outline [&_input]:outline-1 [&_input]:outline-red-600 [&_input]:mb-5"
+    }`,
+  };
+  return (
+    <div
+      className={`flex flex-col sm:flex-row gap-1 items-center justify-center mt-6 ${className.labelChildren} ${className.inputChildren}`}
+    >
+      {label}
+      <div className="flex flex-col justify-center items-center md:justify-start md:items-start">
+        {input}
+        {errorMessage && (
+          <p className={`absolute p-1 text-red-600 translate-y-8 max-w-60`}>
+            {errorMessage}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+export default FormRow;
