9fb23cea712c070e015dd866c169ab786679f377
Bruno <bru-a@hotmail.com>
2024-06-21 22:39:31 -0300

    adjusted footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import "./reset.css";
 import Link from "next/link";
 import { Toaster } from "react-hot-toast";
+import dayjs from "dayjs";
 
 export const metadata: Metadata = {
   title: "Sensei Divino",
@@ -23,28 +24,32 @@ export default function RootLayout({
         />
         {/* Allow react-hot-toast in all application */}
         <Providers>
-          <div className="min-h-screen w-full">
-            <header>
-              <nav className="w-full flex items-center justify-start gap-2">
-                <Link href="/menu" className="w-full max-w-[100px]">
-                  <img
-                    src="/logo-sensei.jpeg"
-                    className="w-full max-w-full block h-auto"
-                  />
-                </Link>
-              </nav>
-            </header>
-            <main className="bg-gradient-to-t from-gray-800 from-0% via-black via-50% to-gray-800 to-100% w-full h-full">
-              {children}
-            </main>
+          <header>
+            <nav className="w-full flex items-center justify-start gap-2">
+              <Link href="/menu" className="w-full max-w-[100px]">
+                <img
+                  src="/logo-sensei.jpeg"
+                  className="w-full max-w-full block h-auto"
+                />
+              </Link>
+            </nav>
+          </header>
+          <main className="bg-gradient-to-t from-gray-800 from-0% via-black via-50% to-gray-800 to-100% min-w-screen min-h-screen">
+            {children}
+          </main>
 
-            <footer className="w-full flex items-center justify-center py-2">
-              <span className="text-xs font-bold text-center text-gray-400">
-                Desenvolvido por alunos da Univesp para a ONG Sensei Divino -
-                &copy; Todos os direitos reservados - 2024
-              </span>
-            </footer>
-          </div>
+          <footer className="w-screen mt-auto p-2 box-border flex flex-col sm:flex-row sm:py-4 sm:gap-2 justify-center items-center">
+            <p className="font-bold text-center text-winePatternDark">
+              Desenvolvido por{" "}
+              <Link href="/creditos">
+                <span className="underline">Togather Group</span>
+              </Link>
+            </p>
+
+            <p className="font-bold text-winePatternDark">
+              &copy; {dayjs().format("YYYY")} Todos os direitos reservados.
+            </p>
+          </footer>
         </Providers>
       </body>
     </html>
