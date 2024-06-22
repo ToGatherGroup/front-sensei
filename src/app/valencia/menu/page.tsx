865bd630a59a52098c38125e7e8040839e598f5b
Bruno <bru-a@hotmail.com>
2024-06-21 22:39:11 -0300

    Remove unused code

--- src/app/valencia/menu/page.tsx
+++ src/app/valencia/menu/page.tsx
@@ -4,14 +4,12 @@ import ListAvaliacao from "@/components/listAvaliacao";
 
 const MenuAvaliacaoPage = () => {
   return (
-    <div className="w-screen h-screen">
-      <div
-        className={
-          "flex-1 flex-col gap-y-2 fadeIn p-8 grid justify-items-center min-h-screen m-auto"
-        }
-      >
-        <ListAvaliacao />
-      </div>
+    <div
+      className={
+        "flex-1 flex-col gap-y-2 fadeIn p-8 grid justify-items-center min-h-screen m-auto"
+      }
+    >
+      <ListAvaliacao />
     </div>
   );
 };
