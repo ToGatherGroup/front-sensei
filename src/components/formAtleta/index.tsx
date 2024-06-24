6971cea25b2dfec48bf4c9b65cb782062c9596ff
Bruno <bru-a@hotmail.com>
2024-06-23 23:38:33 -0300

    Change switch element and style

--- src/components/formAtleta/index.tsx
+++ src/components/formAtleta/index.tsx
@@ -9,35 +9,31 @@ import { atletaCreateSchema } from "@/schemas/athleteSchema";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useAthleteProvider } from "@/contexts";
 import Button from "../ui/button";
-import ReactSwitch from "react-switch";
 
 type Props = {
   atleta?: Atleta | null;
-  method: 'POST' | 'PUT';
+  method: "POST" | "PUT";
 };
 
 const FormAtleta = ({ atleta, method }: Props) => {
+  const switchStyles =
+    ".switch { position: relative; display: inline-block; width: 160px; height: 34px;}.switch input { display: none;}.slider { position: absolute; cursor: pointer; top: 0; left: 0; right: 0; bottom: 0; background-color: #3C3C3C; -webkit-transition: .4s; transition: .4s; border-radius: 34px;}.slider:before { position: absolute; content: ''; height: 26px; width: 26px; left: 4px; bottom: 4px; background-color: white; -webkit-transition: .4s; transition: .4s; border-radius: 50%;}input:checked + .slider { background-color: green;}input:focus + .slider { box-shadow: 0 0 1px #2196F3;}input:checked + .slider:before { -webkit-transform: translateX(26px); -ms-transform: translateX(26px); transform: translateX(125px);}/*------ ADDED CSS ---------*/.slider:after { content: 'Atleta inativo'; color: white; display: block; position: absolute; width: 120px; transform: translate(-50%,-50%); top: 50%; left: 60%; right: 0; font-size: 16px; font-family: Verdana, sans-serif; transition: .4s;}input:checked + .slider:after { content: 'Atleta ativo'; top: 50%; right: 60%; left: 50%; transition: .4s;}";
   const [avatarBase64, setAvatarBase64] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const [disableSubmitBtn, setDisableSubmitBtn] = useState<boolean>(false);
   const { registerAthlete, updateAthlete } = useAthleteProvider();
-  const [checked, setChecked] = useState<boolean>(atleta?.isAtivo ? atleta?.isAtivo : true);
-
-  const handleChange = (nextChecked: boolean) => {
-    setChecked(nextChecked);
-  };
 
   useEffect(() => {
     if (atleta?.foto && avatarBase64 == "") {
       setAvatarBase64(atleta.foto);
     }
+    console.log("atleta:", atleta);
   }, [atleta, setAvatarBase64]);
 
   const {
     register,
     handleSubmit,
     getValues,
-    reset,
     formState: { errors },
   } = useForm({
     defaultValues: {
@@ -46,53 +42,42 @@ const FormAtleta = ({ atleta, method }: Props) => {
       email: atleta?.email ?? "",
       nascimento: atleta?.nascimento ?? "",
       sexo: atleta?.sexo ?? "",
-      peso: atleta?.peso ? atleta?.peso : 0,
-      altura: atleta?.altura ? atleta?.altura : 0,
       faixa: atleta?.faixa,
-      isAtivo: atleta?.isAtivo ? atleta?.isAtivo : true,
+      isAtivo: !!atleta?.isAtivo && true,
     },
     mode: "onBlur",
-    resolver: yupResolver(atletaCreateSchema),  // Certifique-se de que o schema de validação está sendo aplicado corretamente
+    resolver: yupResolver(atletaCreateSchema), // Certifique-se de que o schema de validação está sendo aplicado corretamente
   });
 
   const onSubmit = (data: any) => {
-    switch(method) {
-      case 'PUT': 
+    switch (method) {
+      case "PUT":
         const preparedData = data;
-       
-        if(avatarBase64) {
+
+        if (avatarBase64) {
           preparedData.foto = avatarBase64;
         } else {
           preparedData.foto = atleta?.foto;
         }
-
-        preparedData.isAtivo = checked;
-
+        preparedData.isAtivo = getValues("isAtivo");
+        console.log("preparedData:", preparedData);
         updateAthlete(preparedData);
-        
+
         break;
       default:
         file2Base64(data.foto[0])
-        .then((avatarBase64) => {
-          
-          const preparedData = data;
-
-          preparedData.foto = avatarBase64;
-
-          preparedData.isAtivo = checked;
-         
-          registerAthlete(preparedData);
-        })
-        .catch((error) => {
-          alert(
-            "Houve um erro ao carregar a imagem de avatar. Tente usar uma outra imagem!"
-          );
-          console.log(error);
-        });
+          .then((avatarBase64) => {
+            const preparedData = data;
+            preparedData.foto = avatarBase64;
+            registerAthlete(preparedData);
+          })
+          .catch((error) => {
+            alert(
+              "Houve um erro ao carregar a imagem de avatar. Tente usar uma outra imagem!"
+            );
+            console.log(error);
+          });
     }
-
-    reset();
-    setAvatarBase64("");
   };
 
   const file2Base64 = (file: File): Promise<string> => {
@@ -129,17 +114,12 @@ const FormAtleta = ({ atleta, method }: Props) => {
             title={atleta ? "Editar Atleta" : "Cadastrar Atleta"}
             iconSrc={"/icons/person_24x24_wine.png"}
           ></FormTitle>
-
-          {/* {atleta ? <h1>Alterar Atleta</h1> : <h1>Cadastrar Atleta</h1>}
-          <Image
-            src="/icons/person_24x24.png"
-            alt={atleta ? "Ícone de alteração" : "Ícone de cadastro"}
-            width={24}
-            height={24}
-          /> */}
         </div>
 
-        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <form
+          className={styles.form}
+          onSubmit={handleSubmit(onSubmit, (error) => console.log(error))}
+        >
           <div className={`${styles.inputRow} ${styles.avatar}`}>
             <label htmlFor="photo">
               <p>Foto do atleta</p>
@@ -195,11 +175,7 @@ const FormAtleta = ({ atleta, method }: Props) => {
             <label htmlFor="nascimento" className={styles.required}>
               Data de nascimento
             </label>
-            <input
-              {...register("nascimento")}
-              type="date"
-              id="nascimento"
-            />
+            <input {...register("nascimento")} type="date" id="nascimento" />
             {errors.nascimento && (
               <p className={styles.displayError}>{errors.nascimento.message}</p>
             )}
@@ -209,71 +185,21 @@ const FormAtleta = ({ atleta, method }: Props) => {
             <label htmlFor="sexo" className={styles.required}>
               Sexo
             </label>
-            <select
-              {...register("sexo")}
-              id="sexo"
-            >
+            <select {...register("sexo")} id="sexo">
               <option value="">Selecione</option>
               <option value="M">Masculino</option>
               <option value="F">Feminino</option>
-              <option value="O">Outro</option>
             </select>
             {errors.sexo && (
               <p className={styles.displayError}>{errors.sexo.message}</p>
             )}
           </div>
 
-          <div className={styles.weightHeight}>
-            <div className={styles.inputRow}>
-              <label htmlFor="peso" className={styles.required}>
-                Peso (kg)
-              </label>
-              <input
-                {...register("peso")}
-                type="number"
-                id="peso"
-                placeholder={"0,0"}
-                step={0.01}
-              />
-            </div>
-
-            <div className={styles.inputRow}>
-              <label htmlFor="altura" className={styles.required}>
-                Altura (cm)
-              </label>
-              <input
-                {...register("altura")}
-                type="number"
-                id="altura"
-                placeholder={"0"}
-                step={1}
-              />
-            </div>
-          </div>
-
-          {errors.peso && (
-            <p
-              className={`${styles.displayError} ${styles.displayErrorWidthHeight}`}
-            >
-              {errors.peso.message}
-            </p>
-          )}
-          {errors.altura && (
-            <p
-              className={`${styles.displayError} ${styles.displayErrorWidthHeight}`}
-            >
-              {errors.altura.message}
-            </p>
-          )}
-
           <div className={styles.inputRow}>
             <label htmlFor="faixa" className={styles.required}>
               Faixa
             </label>
-            <select
-              {...register("faixa")}
-              id="faixa"
-            >
+            <select {...register("faixa")} id="faixa">
               <option value="" disabled hidden>
                 Selecione
               </option>
@@ -295,28 +221,19 @@ const FormAtleta = ({ atleta, method }: Props) => {
             )}
           </div>
 
-          <div className={styles.isAtivo}>
-            <label htmlFor="isAtivo" className={styles.required}>
-              Atleta ativo
-            </label>
-            
-            <ReactSwitch
-              {...register("isAtivo")}
-              onChange={handleChange}
-              checked={checked}
-              offColor="#888"
-              onColor="#0f0"
-              uncheckedIcon={false}
-              checkedIcon={false}
-              handleDiameter={20}
-              height={20}
-              width={40}
-            />
-           
-            {errors.isAtivo && (
-              <p className={styles.displayError}>{errors.isAtivo.message}</p>
-            )}
-          </div>
+          {method === "PUT" && (
+            <div className={styles.isAtivo}>
+              <style>{switchStyles}</style>
+              <label className="switch">
+                <input type="checkbox" {...register("isAtivo")} />
+                <span className="slider"></span>
+              </label>
+
+              {errors.isAtivo && (
+                <p className={styles.displayError}>{errors.isAtivo.message}</p>
+              )}
+            </div>
+          )}
 
           <Button
             text={atleta ? "Alterar" : "Cadastrar"}
