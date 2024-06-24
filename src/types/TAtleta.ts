e5c4e13d6c35e577f48df0e1aa7b28a304b68661
Bruno <bru-a@hotmail.com>
2024-06-23 23:37:54 -0300

    Remove peso and altura from type Atleta

--- src/types/TAtleta.ts
+++ src/types/TAtleta.ts
@@ -2,14 +2,12 @@ import { Faixas } from "@/enums/faixas";
 
 export type Atleta = {
   id?: number;
-  peso: number;
-  altura: number;
   nome: string;
   email: string;
   nascimento: string;
   foto: string;
   categoria?: string;
   faixa: (typeof Faixas)[number];
-  sexo: "M" | "F" | "O";
+  sexo: "M" | "F";
   isAtivo?: boolean;
 };
