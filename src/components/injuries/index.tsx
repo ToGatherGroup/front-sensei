ce3eb441f5462927b02b318d4ac56b3420c8b64b
Bruno <bru-a@hotmail.com>
2024-06-25 21:32:16 -0300

    Remove height limit from dummy

--- src/components/injuries/index.tsx
+++ src/components/injuries/index.tsx
@@ -1,28 +1,42 @@
-import React from 'react'
-import Back from '../../../public/svg/injuries/Back';
-import Front from '../../../public/svg/injuries/Front';
+import React from "react";
+import Back from "../../../public/svg/injuries/Back";
+import Front from "../../../public/svg/injuries/Front";
 
 type InjuriesProps = {
   injuries: string[];
-  type: 'back' | 'front';
+  type: "back" | "front";
   width?: string;
   height?: string;
   viewBoxSecondValue?: string;
   viewBoxValue?: string;
-}
+};
 
-const Injuries = ({ injuries, type, width, height, viewBoxValue }: InjuriesProps) => {
-
+const Injuries = ({
+  injuries,
+  type,
+  width,
+  height,
+  viewBoxValue,
+}: InjuriesProps) => {
   return (
-    <div className="max-h-52 lg:max-h-fit">
-      {type === 'back' ? (
-        <Back injuries={injuries} width={width} height={height} viewBoxValue={viewBoxValue} />
+    <div>
+      {type === "back" ? (
+        <Back
+          injuries={injuries}
+          width={width}
+          height={height}
+          viewBoxValue={viewBoxValue}
+        />
       ) : (
-        <Front injuries={injuries} width={width} height={height} viewBoxValue={viewBoxValue} />
+        <Front
+          injuries={injuries}
+          width={width}
+          height={height}
+          viewBoxValue={viewBoxValue}
+        />
       )}
     </div>
-  )
+  );
+};
 
-}
-
-export default Injuries
+export default Injuries;
