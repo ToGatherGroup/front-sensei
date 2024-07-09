284741a9803a1efd6f332814c10040b602baf82c
Bruno <bru-a@hotmail.com>
2024-07-09 20:44:13 -0300

    add new header menu for desktop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import { Providers } from "@/providers";
 import type { Metadata } from "next";
 import "./globals.css";
 import "./reset.css";
-import Link from "next/link";
 import { Toaster } from "react-hot-toast";
-import dayjs from "dayjs";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 export const metadata: Metadata = {
   title: "Sensei Divino",
@@ -24,32 +24,11 @@ export default function RootLayout({
         />
         {/* Allow react-hot-toast in all application */}
         <Providers>
-          <header>
-            <nav className="w-full flex items-center justify-start gap-2">
-              <Link href="/menu" className="w-full max-w-[100px]">
-                <img
-                  src="/logo-sensei.jpeg"
-                  className="w-full max-w-full block h-auto"
-                />
-              </Link>
-            </nav>
-          </header>
+          <Header />
           <main className="px-2.5 py-10 bg-gradient-to-t from-gray-800 from-0% via-black via-50% to-gray-800 to-100% min-w-screen min-h-screen">
             {children}
           </main>
-
-          <footer className="w-screen mt-auto p-2 box-border flex flex-col sm:flex-row sm:py-4 sm:gap-2 justify-center items-center">
-            <p className="font-bold text-center text-winePatternDark">
-              Desenvolvido por{" "}
-              <Link href="/creditos">
-                <span className="underline">Togather Group</span>
-              </Link>
-            </p>
-
-            <p className="font-bold text-winePatternDark">
-              &copy; {dayjs().format("YYYY")} Todos os direitos reservados.
-            </p>
-          </footer>
+          <Footer />
         </Providers>
       </body>
     </html>
---
+++ src/components/header/index.tsx
@@ -0,0 +1,105 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const MENU_ITEMS = [
+  {
+    title: "Cadastro",
+    linkSrc: "/atleta/cadastrar",
+    imgSrc: "/header_icons/add_atletas.png",
+    imgW: 25,
+    imgH: 20,
+  },
+  {
+    title: "Chamada",
+    linkSrc: "/athletes",
+    imgSrc: "/header_icons/chamada.png",
+    imgW: 20,
+    imgH: 20,
+  },
+  {
+    title: "Avaliação",
+    linkSrc: "/valencia/menu",
+    imgSrc: "/header_icons/avaliacao.png",
+    imgW: 30,
+    imgH: 30,
+  },
+  {
+    title: "Relatórios",
+    linkSrc: "/relatorioAvaliacao",
+    imgSrc: "/header_icons/relatorio.png",
+    imgW: 20,
+    imgH: 20,
+  },
+  {
+    title: "Atletas",
+    linkSrc: "/atleta/buscar",
+    imgSrc: "/header_icons/atletas.png",
+    imgW: 30,
+    imgH: 25,
+  },
+  {
+    title: "Comparativo",
+    linkSrc: "/comparison",
+    imgSrc: "/header_icons/versus.png",
+    imgW: 35,
+    imgH: 30,
+  },
+];
+
+const USERNAME = "Bruno Amado Silva Santos";
+
+export default function Header() {
+  return (
+    <>
+      <header className="w-screen box-border h-20 block bg-winePattern">
+        <nav className="flex items-center justify-start">
+          <Link
+            href=""
+            className="h-20 w-fit inline-block ml-2 mr-10 cursor-default flex-shrink-0"
+          >
+            <Image
+              width={90}
+              height={50}
+              alt="Logotipo Sensei"
+              src="/logo_sensei_white.png"
+              className="mt-[15px]"
+            />
+          </Link>
+          {true ? (
+            // Header menu icons (Desktop)
+            <>
+              <div className="flex gap-6">
+                {MENU_ITEMS.map((item) => (
+                  <Link
+                    key={item.title}
+                    href={item.linkSrc}
+                    className="flex justify-center items-center hover:bg-winePatternDark hover:rounded hover:outline hover:outline-1 hover:outline-white p-3"
+                  >
+                    <div className="flex items-center justify-center gap-2">
+                      <Image
+                        src={item.imgSrc}
+                        width={item.imgW}
+                        height={item.imgH}
+                        alt={item.title}
+                      />
+                      <span className="text-white h-fit">{item.title}</span>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+              <div className="text-white mr-4 flex flex-col gap-2 ml-auto p-2 whitespace-nowrap min-w-[120px]">
+                <p className="truncate">Olá, {USERNAME}</p>
+                <Link href="#" className="ml-auto">
+                  Logout
+                </Link>
+              </div>
+            </>
+          ) : (
+            // sanduiche
+            <div className="size-4 bg-gray-400 ml-auto" />
+          )}
+        </nav>
+      </header>
+    </>
+  );
+}
