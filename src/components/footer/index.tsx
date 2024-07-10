a3e489545f8ad4e9e64497c6b7777c0e38bbeb18
Bruno Amado <bru-a@hotmail.com>
2024-07-10 00:05:33 -0300

    change footer bg-color to wineSensei

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,15 +3,15 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="w-screen mt-auto p-2 box-border flex flex-col sm:flex-row sm:py-4 sm:gap-2 justify-center items-center">
-      <p className="font-bold text-center text-winePatternDark">
+    <footer className="w-screen mt-auto p-2 box-border flex flex-col sm:flex-row sm:py-4 sm:gap-2 justify-center items-center bg-winePattern">
+      <p className="font-bold text-center text-white">
         Desenvolvido por{" "}
         <Link href="/creditos">
           <span className="underline">Togather Group</span>
         </Link>
       </p>
 
-      <p className="font-bold text-winePatternDark">
+      <p className="font-bold text-white">
         &copy; {dayjs().format("YYYY")} Todos os direitos reservados.
       </p>
     </footer>
