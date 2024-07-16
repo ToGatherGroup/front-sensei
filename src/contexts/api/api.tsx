c887ba566cff1bc4f8bf986003f68531ed723bdd
Bruno <bru-a@hotmail.com>
2024-07-16 00:07:50 -0300

    fixed <Loader/> component

--- src/contexts/api/api.tsx
+++ src/contexts/api/api.tsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState } from "react";
 import axios, { AxiosResponse } from "../../../node_modules/axios/index";
 import toast from "react-hot-toast";
-import Loading from "@/components/loading/index";
+import Loader from "@/components/loading/index";
 
 // URL FOR ALL REQUESTS:
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
@@ -186,7 +186,7 @@ export const ApiProvider = ({ children }: { children: React.ReactNode }) => {
     <ApiContext.Provider
       value={{ get, post, put, patch, remove, isLoadingAPI }}
     >
-      {isLoadingAPI && <Loading />}
+      {isLoadingAPI && <Loader />}
       {children}
     </ApiContext.Provider>
   );
