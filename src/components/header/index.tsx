1bbaa1742ea5b0da7b738b743b37c65757cffe33
Bruno Amado <bru-a@hotmail.com>
2025-02-06 02:01:34 -0300

    starting dropdown header menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -32,6 +32,43 @@ const MENU_ITEMS = [
     imgSrc: "/header_icons/relatorio.png",
     imgW: 20,
     imgH: 20,
+    childrens: [
+      {
+        title: "Relatórios 1",
+    linkSrc: "/relatorioAvaliacao",
+    imgSrc: "/header_icons/relatorio.png",
+    imgW: 20,
+    imgH: 20,
+      },
+      {
+        title: "Relatórios 2",
+    linkSrc: "/relatorioAvaliacao",
+    imgSrc: "/header_icons/relatorio.png",
+    imgW: 20,
+    imgH: 20,
+      },
+      {
+        title: "Relatórios 3",
+    linkSrc: "/relatorioAvaliacao",
+    imgSrc: "/header_icons/relatorio.png",
+    imgW: 20,
+    imgH: 20,
+      },
+      {
+        title: "Relatórios 4",
+    linkSrc: "/relatorioAvaliacao",
+    imgSrc: "/header_icons/relatorio.png",
+    imgW: 20,
+    imgH: 20,
+      },
+      {
+        title: "Relatórios 5",
+    linkSrc: "/relatorioAvaliacao",
+    imgSrc: "/header_icons/relatorio.png",
+    imgW: 20,
+    imgH: 20,
+      },
+    ]
   },
   {
     title: "Atletas",
@@ -85,14 +122,15 @@ export default function Header() {
             } animate-duration-75 fixed z-[99999] flex flex-col top-0 lg:right-0 bg-winePatternDark min-h-screen rounded-bl-md px-5 py-10 lg:min-h-0 lg:h-fit lg:static lg:flex-row lg:bg-transparent lg:rounded-none lg:p-0`}
           >
             {MENU_ITEMS.map((item) => (
+              <div key={item.title} className="group">
               <Link
                 key={item.title}
-                href={item.linkSrc}
-                className="flex justify-center items-center hover:bg-winePatternLight lg:hover:bg-winePatternDark hover:rounded hover:outline hover:outline-1 hover:outline-white py-3 px-5"
-                onClick={() => setShowMobileMenu(false)}
+                href={item.childrens?.length ? '#' : item.linkSrc}
+                className="z-20 peer flex justify-center hover:bg-winePatternLight lg:hover:bg-winePatternDark rounded border border-solid border-transparent hover:border hover:border-white py-3 px-5"
+                onClick={() => !item.childrens?.length && setShowMobileMenu(false)}
               >
-                <div className="flex items-center justify-center gap-2">
-                  <div className="w-[30px] h-[30px] flex justify-center items-center">
+                <div className="flex justify-center gap-2">
+                  <div className="w-[30px] h-[30px] flex justify-center">
                     <Image
                       src={item.imgSrc}
                       width={item.imgW}
@@ -106,6 +144,36 @@ export default function Header() {
                   </span>
                 </div>
               </Link>
+              {item.childrens &&
+                <div className="z-10 hidden bg-winePattern peer-hover:block hover:block lg:absolute rounded-b-md lg:p-1 lg:-translate-x-1 shadow-2xl">
+                  {
+                    item.childrens.map((children) => (
+                      <Link
+                        key={children.title}
+                        href={children.linkSrc}
+                        className="flex justify-center hover:bg-winePatternLight lg:hover:bg-winePatternDark hover:rounded hover:outline hover:outline-1 hover:outline-white py-3 px-5"
+                        onClick={() => setShowMobileMenu(false)}
+                      >
+                        <div className="flex justify-center gap-2">
+                          <div className="w-[30px] h-[30px] flex justify-center">
+                            <Image
+                              src={children.imgSrc}
+                              width={children.imgW}
+                              height={children.imgH}
+                              alt={children.title}
+                              className="m-auto"
+                            />
+                          </div>
+                          <span className="text-white h-fit w-28 text-center lg:w-fit lg:text-start">
+                            {children.title}
+                          </span>
+                        </div>
+                      </Link>
+                    ))
+                  }
+                </div>
+              }
+              </div>
             ))}
 
             {/* Mobile return menu */}
