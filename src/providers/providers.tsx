58123b646864d2179d6ea8b0ffafc00502b39997
Bruno Amado <bru-a@hotmail.com>
2025-02-10 23:30:48 -0300

    lib: add @iconify-json/hugeicons

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -5,6 +5,10 @@ import { AthleteProvider } from "@/contexts";
 import { ThemeProvider } from "@mui/material";
 import { muiGlobalTheme } from "@/components/ui/mui/globalTheme";
 import { Toaster } from "react-hot-toast";
+import { addCollection } from "@iconify/react";
+import * as hugeicons from "@iconify-json/hugeicons";
+
+addCollection(hugeicons.icons);
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
