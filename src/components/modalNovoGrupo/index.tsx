771614dbc296dcd827262eebd32fb865a6345ba9
Paulo Mota <paulo.kiin@live.com>
2025-02-25 17:21:12 -0300

    newGroupModal Update

--- src/components/modalNovoGrupo/index.tsx
+++ src/components/modalNovoGrupo/index.tsx
@@ -6,58 +6,82 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
+import { useGroupProvider } from '@/contexts/groups/groups';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
+interface ModalNewGroupProps {
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    onGroupCreated?: () => void;
+}
 
-export default function ModalDialog() {
-    const [open, setOpen] = React.useState(false);
+// Schema de validação para o formulário do modal
+const groupSchema = yup.object().shape({
+    nome: yup.string().required('Nome do grupo é obrigatório')
+});
 
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+export default function ModalNewGroup({ open, setOpen, onGroupCreated }: ModalNewGroupProps) {
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        const inputText = formData.get('inputText') as string;
+    const { postGroup } = useGroupProvider();
+    const [name, setName] = useState<string>('');
 
-        // Lógica para requisição futura
-        console.log('Dados submetidos:', inputText);
+    // Configuração do react-hook-form
+    const {
+        formState: { errors },
+        reset
+    } = useForm({
+        resolver: yupResolver(groupSchema)
+    });
+
+    const handleClose = () => {
+        reset();
+        setOpen(false);
+    };
+
+    const handleCreateGroup = async () => {
+        if (name) {
+            postGroup(name).then(() => {
+                onGroupCreated && onGroupCreated(); // Callback opcional para o pai saber que um grupo foi criado
+            })
+        }
         handleClose();
     };
 
     return (
         <div>
-            <Dialog open={true} onClose={handleClose}>
-                <form onSubmit={handleSubmit}>
-                    <DialogTitle>Criar novo grupo</DialogTitle>
-                    <DialogContent>
-                        <TextField
-                            sx={(theme) => (
-                                {
-                                    '& .MuiInputLabel-root': {
-                                        borderColor: theme.palette.primary.main,
-                                        borderWidth: 12,
-                                    },
-                                    '& .MuiOutlinedInput-notchedOutline': {
-                                        borderColor: theme.palette.secondary.main,
-                                        borderWidth: 2,
-                                    },
-                                }
-                            )}
-                            autoFocus
-                            fullWidth
-                            margin="dense"
-                            id="inputField"
-                            name="inputText"
-                            label="Nome do novo grupo"
-                            variant="outlined"
-                        />
-                    </DialogContent>
-
-                    <DialogActions sx={{ justifyContent: 'space-around' }}>
-                        <Button onClick={handleClose} disableElevation  sx={{ color: 'red' }}>Cancelar</Button>
-                        <Button type="submit" variant='contained' disableElevation>Criar Grupo</Button>
-                    </DialogActions>
-                </form>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Criar novo grupo</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        sx={(theme) => (
+                            {
+                                '& .MuiInputLabel-root': {
+                                    borderColor: theme.palette.primary.main,
+                                    borderWidth: 12,
+                                },
+                                '& .MuiOutlinedInput-notchedOutline': {
+                                    borderColor: theme.palette.secondary.main,
+                                    borderWidth: 2,
+                                },
+                            }
+                        )}
+                        autoFocus
+                        fullWidth
+                        margin="dense"
+                        id="inputField"
+                        name="inputText"
+                        label="Nome do novo grupo"
+                        variant="outlined"
+                        onChange={(e) => setName(e.target.value.trim?.())}
+                    />
+                </DialogContent>
+                <DialogActions sx={{ justifyContent: 'space-around' }}>
+                    <Button onClick={handleClose} disableElevation sx={{ color: 'red' }}>Cancelar</Button>
+                    <Button onClick={handleCreateGroup} variant='contained' sx={{ color: 'red' }} disableElevation>Criar Grupo</Button>
+                </DialogActions>
             </Dialog>
         </div>
     );
