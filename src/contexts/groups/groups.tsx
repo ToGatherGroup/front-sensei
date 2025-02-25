5b2d1b17ec6bf7495ef635ab10450d4b0665d532
Paulo Mota <paulo.kiin@live.com>
2025-02-25 17:19:46 -0300

    new context groups

---
+++ src/contexts/groups/groups.tsx
@@ -0,0 +1,93 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { useApiProvider } from "../api/api";
+import { Grupo } from "@/types/Grupo";
+
+
+type GroupState = {
+    groupList: Grupo[] | []; // Array vazio como fallback
+    getGroups: () => Promise<void>;
+    postGroup: (name: string) => Promise<void>;
+};
+
+const initialState = {
+    groupList: [] as Grupo[], // Inicializando como array vazio
+    getGroups: async () => {},
+    postGroup: async () => {}
+};
+
+const GroupContext = createContext<GroupState>(initialState);
+
+export const GroupProvider = ({
+    children,
+}: {
+    children: React.ReactNode;
+}) => {
+    const { get, post, put } = useApiProvider();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [groupList, setGroupList] = useState<Grupo[]>([]); 
+
+    useEffect(() => {
+        getGroups();
+      }, []);
+
+      const getGroups = async () => {
+        setIsLoading(true);
+        try {
+          const response = await get(`/grupo`);
+          // Verificando se a resposta contém um array antes de atualizar o estado
+          if (Array.isArray(response?.data)) {
+            setGroupList(response.data);
+            console.log("Requisição feita para /grupo", response.data);
+          } else {
+            console.error("Resposta da API não é um array:", response?.data);
+            setGroupList([]);  // Define como array vazio em caso de erro
+          }
+        } catch (error) {
+          console.error("Erro ao obter lista de grupos", error);
+          setGroupList([]);  // Define como array vazio em caso de erro
+        } finally {
+          setIsLoading(false);
+        }
+      };
+
+    const putGroup = async (id: number) => {
+        setIsLoading(true);
+        try {
+            const response = await put(`/grupo`, {id});
+            console.log("Requisição do tipo PUT feita para /grupo", response?.data)
+            getGroups();
+        } catch (error) {
+            console.error("Erro ao obter lista de grupos", error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const postGroup = async (name: string) => {
+        setIsLoading(true);
+        try {
+            const response = await post(`/grupo`, name);
+            setGroupList(response?.data);
+            console.log("Requisição do tipo POST feita para /grupo", {name})
+            //getGroups();
+        } catch (error) {
+            console.error("Erro ao obter lista de grupos", error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    return (
+        <GroupContext.Provider
+            value={{
+                groupList,
+                getGroups,
+                postGroup
+            }}
+        >
+            {children}
+        </GroupContext.Provider>
+    );
+};
+
+export const useGroupProvider = () => useContext(GroupContext);
